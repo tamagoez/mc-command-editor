aac6c25acee16f233c8e764e63b3706f1dbc8a23
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-25 00:13:40 +0900

    Update

--- pages/edit/[id].js
+++ pages/edit/[id].js
@@ -1,13 +1,1 @@
 // https://zenn.dev/trictrac/articles/4377beaf0953856b7a0e
-
-import dynamic from "next/dynamic";
-
-const Editor = dynamic(import("../components/Editor/index"), { ssr: false });
-
-export default function Home() {
-  return (
-    <div className='container'>
-      <Editor />
-    </div>
-  );
-}
